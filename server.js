041106d78c789771f6f896f28a8c23aaa39de794
coder-blackhat <admiregeorge14@gmail.com>
2026-04-30 21:32:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,39 @@ app.get('/', (req, res) => {
                     <div style="text-align:center;">
                         <h2 style="color:#fff;font-family:sans-serif;">Scan with WhatsApp</h2>
                         <img src="${qrCode}" style="border:10px solid #fff;width:300px;">
+                        <p style="color:#888;font-family:sans-serif;">CODER_WHITEHAT-MD</p>
+                    </div>
+                </body>
+            </html>
+        `)
+    } else if (isConnected) {
+        res.send('<h1 style="font-family:sans-serif;text-align:center;margin-top:50px;">Bot is connected to WhatsApp!</h1>')
+    } else {
+        res.send('<h1 style="font-family:sans-serif;text-align:center;margin-top:50px;">Starting bot... Refresh in 10s</h1>')
+    }
+})
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+    connectToWhatsApp()
+})            console.log('Bot connected to WhatsApp!')
+            isConnected = true
+            qrCode = ''
+        }
+    })
+
+    sock.ev.on('creds.update', saveCreds)
+}
+
+app.get('/', (req, res) => {
+    if (qrCode && !isConnected) {
+        res.send(`
+            <html>
+                <body style="background:#111;display:flex;justify-content:center;align-items:center;height:100vh;margin:0;">
+                    <div style="text-align:center;">
+                        <h2 style="color:#fff;font-family:sans-serif;">Scan with WhatsApp</h2>
+                        <img src="${qrCode}" style="border:10px solid #fff;width:300px;">
                     </div>
                 </body>
             </html>
