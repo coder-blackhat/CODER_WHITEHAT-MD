1284fcb3b15f2d0b1cee7b18e0da9a10bfff1417
coder-blackhat <admiregeorge14@gmail.com>
2026-05-04 22:26:57 +0200

    Delete commands directory

--- commands/getlink.js
+++
@@ -1,9 +0,0 @@
-export default {
-    name: "getlink",
-    aliases: ["link", "grouplink"],
-    run: async ({ sock, m, from }) => {
-        if (!m.key.remoteJid.endsWith('@g.us')) return await sock.sendMessage(from, { text: "This command only works in groups" })
-        const code = await sock.groupInviteCode(from)
-        await sock.sendMessage(from, { text: `https://chat.whatsapp.com/${code}` })
-    }
-}
--- commands/ping.js
+++
@@ -1,6 +0,0 @@
-export default {
-    name: "ping",
-    run: async ({ sock, m, from }) => {
-        await sock.sendMessage(from, { text: "Pong! 🏓" });
-    }
-}
